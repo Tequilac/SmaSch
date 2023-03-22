603e383f17e239371e9ba0426a888f3f65ded355
Tequilac <ptekielak@gmail.com>
2023-03-22 20:18:33 +0100

    Fix

--- src/validator.py
+++ src/validator.py
@@ -94,8 +94,10 @@ class Validator:
             allocatable = node.status.allocatable
             capacity = node.status.capacity
 
-            free_cpu = int(capacity['cpu']) * 1000 - int(allocatable['cpu'].split('m')[0])
-            free_memory = int(capacity['memory'].split('Ki')[0]) - int(allocatable['memory'].split('Ki')[0])
+            free_cpu = (int(capacity['cpu']) * 1000 - int(allocatable['cpu'].split('m')[0])) / int(
+                allocatable['cpu'].split('m')[0]) * 100
+            free_memory = (int(capacity['memory'].split('Ki')[0]) - int(allocatable['memory'].split('Ki')[0])) / int(
+                allocatable['memory'].split('Ki')[0]) * 100
 
             if free_memory < self._thresholds.free_memory.medium:
                 labels['sma-mem'] = 'sma-mem-low'
